6c0b8f785aa5b759dd8bcf6608952b553d35bfa7
rod <rod@snapzio.com>
2018-08-01 22:43:50 +0800

    setup linting

---
+++ examples/server/index.js
@@ -0,0 +1,18 @@
+const feathers = require('@feathersjs/feathers');
+const mongoose = require('mongoose');
+const services = require('./services');
+
+const { Server } = require('../../lib');
+
+mongoose.Promise = global.Promise;
+
+mongoose.connect('mongodb://localhost:27017/feathers-mq', { useNewUrlParser: true });
+
+const app = feathers();
+
+app.set('name', 'logger');
+
+app.configure(services);
+app.configure(Server());
+
+console.log('running instance with appName:', app.get('name'));
