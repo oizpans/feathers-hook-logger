2085d07efcf37ddea09de92fac65789ac3df922f
rod <rod@snapzio.com>
2018-08-01 23:10:02 +0800

    updated

--- examples/server/index.js
+++ examples/server/index.js
@@ -1,18 +1,11 @@
+const debug = require('debug')('hooks-logger:');
 const feathers = require('@feathersjs/feathers');
-const mongoose = require('mongoose');
 const services = require('./services');
-
-const { Server } = require('../../lib');
-
-mongoose.Promise = global.Promise;
-
-mongoose.connect('mongodb://localhost:27017/feathers-mq', { useNewUrlParser: true });
 
 const app = feathers();
 
-app.set('name', 'logger');
+app.set('name', 'some_app');
 
 app.configure(services);
-app.configure(Server());
 
-console.log('running instance with appName:', app.get('name'));
+debug('running instance with appName:', app.get('name'));
