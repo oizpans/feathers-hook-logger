aa131f44e5fba27cc193416f98936e526be6e946
rod <rod@snapzio.com>
2018-08-01 23:58:36 +0800

    updated readme

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # feathers-hook-logger
-The hook that sends data to our logger service
+The hook that sends data to our logger service.
+
+
+# ROADMAPS
+ - testing
+ - to be used on browsers and mobile ( react native )
--- examples/server/services/products/hooks.js
+++ examples/server/services/products/hooks.js
@@ -1,16 +1,7 @@
+const logger = require('../../../../lib/');
+
 module.exports = {
-  before: {
-    remove: [
-      function remove(context) {
-        // console.log(Object.keys(context.params));
-        return Promise.resolve(context);
-      },
-    ],
-    patch: [
-      function patch(context) {
-        // console.log(Object.keys(context.params));
-        return Promise.resolve(context);
-      },
-    ],
-  },
+  after: [
+    logger(),
+  ],
 };
